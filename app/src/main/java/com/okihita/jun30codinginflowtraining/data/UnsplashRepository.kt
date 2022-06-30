43be023aeb6a1b87f46db14ddcdf6e16c7ef5bca
Okihita Sihaloho <okihita@gmail.com>
2022-06-30 16:15:08 +0700

    Added PagingSource

--- app/src/main/java/com/okihita/jun30codinginflowtraining/data/UnsplashRepository.kt
+++ app/src/main/java/com/okihita/jun30codinginflowtraining/data/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.okihita.jun30codinginflowtraining.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.okihita.jun30codinginflowtraining.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -8,4 +11,16 @@ import javax.inject.Singleton
 class UnsplashRepository @Inject constructor(
     private val unsplashApi: UnsplashApi
 ) {
+
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                UnsplashPagingSource(unsplashApi, query)
+            }
+        ).liveData
 }
--- app/src/main/java/com/okihita/jun30codinginflowtraining/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/okihita/jun30codinginflowtraining/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,24 @@
 package com.okihita.jun30codinginflowtraining.ui.gallery
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.okihita.jun30codinginflowtraining.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    private val galleryVM by viewModels<GalleryViewModel>()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        galleryVM.photos.observe(viewLifecycleOwner) {
+            Log.d("Xena", "onViewCreated: Hello")
+        }
+    }
 }
